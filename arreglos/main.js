9e9dc89f9e6df4687fbbe516400a7c8bd3b1e685
angeloLuna <angel.luna.najera@gmail.com>
2019-01-28 20:05:54 -0600

    arreglos

---
+++ arreglos/main.js
@@ -0,0 +1,48 @@
+
+// let numero = 20
+// // constantes no se pueden modificar
+// const precio = 100
+// precio = 50
+
+// // las variables se pueden sobreescribir
+// numero = numero * numero
+
+// console.log(precio)
+
+// let transportes = ['autos', 'bicicletas', 'autobuses', 'grin']
+
+
+// transportes[4] = 'patineta'
+
+// console.log(transportes)
+
+// Challenge:
+// nombre
+// apellido
+// email
+// edad
+
+// guardarlos en un arreglo
+// imprimir el arreglo completo
+// imprimir cada uno (del arreglo)
+
+let nombre = prompt('nombre')
+let apellido = prompt('apellido')
+let email = prompt('email')
+let edad = prompt('edad')
+
+let datos = []
+
+datos[0] = nombre
+datos[1] = apellido
+datos[2] = email
+datos[3] = edad
+
+console.log(datos)
+
+console.log(datos[0])
+console.log(datos[1])
+console.log(datos[2])
+console.log(datos[3])
+
+
